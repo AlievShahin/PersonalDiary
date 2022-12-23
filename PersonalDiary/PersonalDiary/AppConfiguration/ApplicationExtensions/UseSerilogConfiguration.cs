1e2cdf6aeadb22f47f44a2448b7f50a28f8fd74f
AlievShahin <107402093+AlievShahin@users.noreply.github.com>
2022-12-23 20:06:02 +0300

    add task2 full

---
+++ PersonalDiary/PersonalDiary/AppConfiguration/ApplicationExtensions/UseSerilogConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using Serilog;
+
+namespace PersonalDiary.AppConfiguration.ApplicationExtensions
+{
+
+    public static partial class AppExtensions
+    {
+        /// <summary>
+        /// Use serilog configuration
+        /// </summary>
+        /// <param name="app"></param>
+        public static void UseSerilogConfiguration(this IApplicationBuilder app)
+        {
+            app.UseSerilogRequestLogging();
+        }
+    }
+}
